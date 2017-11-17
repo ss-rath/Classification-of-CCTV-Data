383f02120353c94aeb57a83a357d2d5e8ec8c9eb
Soumya Sambit Rath <rath.soumyasambit@gmail.com>
2017-11-17 23:45:48 +0530

    initial commit

---
+++ install_guide.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+#installing cmake
+sudo apt-get install -y build-essential cmake
+
+#installing the required essential libraries
+sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+sudo apt-get install -y libtbb-dev libeigen3-dev
+
+#installing python
+sudo apt-get install -y python-dev python-tk python-numpy 
+
+#installing openCV
+sudo apt-get install -y unzip wget
+wget https://github.com/opencv/opencv/archive/3.2.0.zip
+unzip 3.2.0.zip
+rm 3.2.0.zip
+mv opencv-3.2.0 OpenCV
+cd OpenCV
+mkdir build
+cd build
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DENABLE_PRECOMPILED_HEADERS=OFF ..
+make -j4
+sudo make install
+sudo ldconfig
